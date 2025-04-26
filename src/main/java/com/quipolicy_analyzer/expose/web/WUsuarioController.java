27156ba1783db97c35edec43fe86b399eb95be88
Driany2003 <creo07@hotmail.com>
2025-04-26 02:14:51 -0500

    pruebas unitarias

--- src/main/java/com/quipolicy_analyzer/expose/web/WUsuarioController.java
+++ src/main/java/com/quipolicy_analyzer/expose/web/WUsuarioController.java
@@ -3,7 +3,8 @@ package com.quipolicy_analyzer.expose.web;
 import com.quipolicy_analyzer.business.IUsuarioService;
 import com.quipolicy_analyzer.model.api.usuario.UsuarioRequest;
 import com.quipolicy_analyzer.model.api.usuario.UsuarioResponse;
-import com.quipolicy_analyzer.model.api.usuario.dto.Usua_auth_listar_Response;
+import com.quipolicy_analyzer.model.api.usuario.dto.Usua_auth_Request;
+import com.quipolicy_analyzer.model.api.usuario.dto.Usua_auth_Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import com.quipolicy_analyzer.util.funciones.FxComunes;
 import java.util.List;
 
 @RestController
+@RequestMapping("/usuario")
 @Slf4j
 public class WUsuarioController {
 
@@ -21,39 +23,39 @@ public class WUsuarioController {
   private IUsuarioService usuarioService;
 
   @PostMapping("/create")
-  public ResponseEntity<UsuarioResponse> create (@RequestBody UsuarioRequest request){
+    public ResponseEntity<Usua_auth_Response> create (@RequestBody Usua_auth_Request request){
     FxComunes.printJson("Datos del usuario request: ", request);
-    UsuarioResponse response = usuarioService.create(request);
+    Usua_auth_Response response = usuarioService.create(request);
     return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
 
   @GetMapping("/find-by-id/{usuaId}")
-  public ResponseEntity<UsuarioResponse> findById (@PathVariable Integer usuaId ){
+  public ResponseEntity<Usua_auth_Response> findById (@PathVariable Integer usuaId ){
     log.info("Controller :: find-by-id :: {}", usuaId);
-    UsuarioResponse response = usuarioService.findById(usuaId);
+    Usua_auth_Response response = usuarioService.findById(usuaId);
     return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
 
   @GetMapping("/findAll")
-  public ResponseEntity<List<Usua_auth_listar_Response>> findAll(){
+  public ResponseEntity<List<Usua_auth_Response>> findAll(){
     log.info("Controller :: findAll");
-    List<Usua_auth_listar_Response> response = usuarioService.findAll();
+    List<Usua_auth_Response> response = usuarioService.findAll();
     return new ResponseEntity<>(response,HttpStatus.OK);
   }
 
   @PostMapping("/delete/{usuaId}")
-  public ResponseEntity<UsuarioResponse> delete (@PathVariable Integer usuaId){
+  public ResponseEntity<Usua_auth_Response> delete (@PathVariable Integer usuaId){
     log.info("Controller :: delete :: {}", usuaId);
-    UsuarioResponse response = usuarioService.delete(usuaId);
+    Usua_auth_Response response = usuarioService.delete(usuaId);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
 
   @PostMapping("/update")
-  public ResponseEntity<UsuarioResponse> update (@RequestBody UsuarioRequest request){
+  public ResponseEntity<Usua_auth_Response> update (@RequestBody Usua_auth_Request request){
     log.info("Controller :: update :: {}", request.getUsuaId());
     FxComunes.printJson("Datos del usuario a actualizar request: ", request);
-    UsuarioResponse response = usuarioService.update(request);
+    Usua_auth_Response response = usuarioService.update(request);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 }
